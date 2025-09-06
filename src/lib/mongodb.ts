c61cff51b2b1ca86f08ee634f4ab45a15900e59c
Ayush Agrawal <Aayush.2005sng@gmail.com>
2025-09-06 06:58:02 +0000

    problem is not fixed

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,20 +1,13 @@
 
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import { MongoClient } from 'mongodb'
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
 
 const uri = process.env.MONGODB_URI
-const options = {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-  tls: true,
-}
+const options = {}
 
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
