bd2e311c71c95b432328b8fc86ae0bc7a60328e1
Ayush Agrawal <Aayush.2005sng@gmail.com>
2025-09-07 17:11:30 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,7 +7,6 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI
-const options = {}
 
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
@@ -20,13 +19,13 @@ if (process.env.NODE_ENV === 'development') {
   }
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(uri)
     globalWithMongo._mongoClientPromise = client.connect()
   }
   clientPromise = globalWithMongo._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
+  client = new MongoClient(uri)
   clientPromise = client.connect()
 }
 
